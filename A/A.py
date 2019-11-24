2794509c452d4b962d39b4035d91558b3719f0db
sho Taguchi <s.taguchi0703@gmail.com>
2019-11-25 00:43:02 +0900

    BをAC

--- A/A.py
+++ A/A.py
@@ -16,6 +16,25 @@ sys.stdin=f
 ##################################
 # %%
 # 以下ペースト可
-num_list = [int(item) for item in input().split()]
-print('A', num_list)
+x, y = [int(item) for item in input().split()]
 
+res = 0
+
+if x == 1:
+    res += 300000
+elif x == 2:
+    res += 200000
+elif x == 3:
+    res += 100000
+
+if y == 1:
+    res += 300000
+elif y == 2:
+    res += 200000
+elif y == 3:
+    res += 100000
+
+if x == 1 and y == 1:
+    res += 400000
+
+print(res)
--- B/B.py
+++ B/B.py
@@ -16,6 +16,17 @@ sys.stdin=f
 ##################################
 # %%
 # 以下ペースト可
-num_list = [int(item) for item in input().split()]
-print('B', num_list)
 
+N = int(input())
+A_list = [int(item) for item in input().split()]
+
+all_sum = sum(A_list)
+
+F_sum_list = [A_list[0]]
+
+for j in range(1,N):
+    F_sum_list.append(F_sum_list[-1] + A_list[j])
+
+delta_list = [abs(all_sum - 2* i) for i in F_sum_list]
+
+print(min(delta_list))
